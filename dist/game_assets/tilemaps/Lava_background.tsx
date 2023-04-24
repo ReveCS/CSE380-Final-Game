1e5d720a1a5b0b9c12fe725760e8814e105728ba
Avery <averychan0320@gmail.com>
2023-04-24 16:30:00 -0400

    implemented level3&4 background

---
+++ dist/game_assets/tilemaps/Lava_background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Lava level" tilewidth="16" tileheight="16" tilecount="3750" columns="75">
+ <image source="Lava level.png" width="1200" height="800"/>
+</tileset>
